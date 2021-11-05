7c0f0fa453b35caca5507c61ff7f6f02467718fb
Denise-Phi Khuu <denise-phi.khuu@capgemini.com>
2021-11-05 11:42:35 +0100

    search engine

---
+++ temp/pages/tutorials/index.js
@@ -0,0 +1,8 @@
+function main() {
+
+    var seachFieldDiv = '<div class="search-bar"><input id="search-field-solutions" type="search" class="form-control mr-sm-2" placeholder="Search by keyword(s)..." aria-label="Search" style="height: auto;"/></div>';
+    $('#content').append(seachFieldDiv);
+	
+}
+
+main()
